42734e63c83a0c3c01f9b89792680396dc531a77
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-10-12 12:33:55 +0400

    Import from agent (#1)
    * import from agent
    
    * add context category and update sdk version
    
    * add min versions for agent and instance
    
    * add info about agent
    
    * add info about agent

--- src/globals.py
+++ src/globals.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import supervisely as sly
 
 from distutils.util import strtobool
-# from dotenv import load_dotenv
 
 app_root_directory = str(Path(__file__).parent.absolute().parents[0])
 sly.logger.info(f"App root directory: {app_root_directory}")
 
+# debug
+# from dotenv import load_dotenv
 # load_dotenv(os.path.join(app_root_directory, "debug.env"))
 # load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
 
@@ -23,6 +24,11 @@ INPUT_FILE = os.environ.get("modal.state.slyFile")
 OUTPUT_PROJECT_NAME = os.environ.get("modal.state.project_name", "")
 REMOVE_SOURCE = bool(strtobool(os.getenv("modal.state.remove_source")))
 assert INPUT_DIR or INPUT_FILE
+
+if INPUT_DIR:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
+else:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
 storage_dir = my_app.data_dir
 sly.fs.mkdir(storage_dir, True)
