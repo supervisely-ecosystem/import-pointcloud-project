5d04db9fb927f08832c82de95f7abdabcbe37154
almaz <almazgimaev@gmail.com>
2023-11-14 15:03:37 +0100

    handle corner cases, better logs

--- src/functions.py
+++ src/functions.py
@@ -1,17 +1,114 @@
+import functools
 import os
-import globals as g
 import tarfile
 import zipfile
-from pathlib import Path
+from os.path import basename, dirname, normpath
+from typing import Callable
 
+import globals as g
 import download_progress
 import supervisely as sly
+from supervisely.io.fs import get_file_name_with_ext, silent_remove, get_file_ext
+
+
+def update_progress(count, api: sly.Api, task_id: int, progress: sly.Progress) -> None:
+    count = min(count, progress.total - progress.current)
+    progress.iters_done(count)
+    if progress.need_report():
+        progress.report_progress()
+
+
+def get_progress_cb(
+    api: sly.Api,
+    task_id: int,
+    message: str,
+    total: int,
+    is_size: bool = False,
+    func: Callable = update_progress,
+) -> functools.partial:
+    progress = sly.Progress(message, total, is_size=is_size)
+    progress_cb = functools.partial(func, api=api, task_id=task_id, progress=progress)
+    progress_cb(0)
+    return progress_cb
 
 
 def get_project_name_from_input_path(input_path: str) -> str:
     """Returns project name from target sly folder name."""
     full_path_dir = os.path.dirname(input_path)
     return os.path.basename(full_path_dir)
+
+
+def search_projects(dir_path):
+    files = os.listdir(dir_path)
+    meta_exists = "meta.json" in files
+    datasets = [f for f in files if sly.fs.dir_exists(os.path.join(dir_path, f))]
+    datasets_exists = len(datasets) > 0
+    return meta_exists and datasets_exists
+
+
+def search_pcd_dir(dir_path):
+    listdir = os.listdir(dir_path)
+    is_pcd_dir = any(
+        get_file_ext(f) in sly.pointcloud.ALLOWED_POINTCLOUD_EXTENSIONS for f in listdir
+    )
+    return is_pcd_dir
+
+
+def is_archive_path(path):
+    return get_file_ext(path) in [".zip", ".tar"] or path.endswith(".tar.gz")
+
+
+def check_input_path(api, input_dir: str, input_file: str) -> str:
+    """Checks the input path."""
+
+    if input_dir:
+        listdir = api.file.listdir(g.TEAM_ID, input_dir)
+        archives_cnt = len([file for file in listdir if is_archive_path(file)])
+        if archives_cnt > 1:
+            raise Exception("Multiple archives are not supported.")
+        if len(listdir) == 1 and archives_cnt == 1:
+            sly.logger.info(
+                "Folder mode is selected, but archive file is uploaded. Switching to file mode."
+            )
+            input_dir, input_file = None, listdir[0]
+
+    if input_file:
+        file_ext = get_file_ext(input_file)
+        if not is_archive_path(input_file):
+            sly.logger.info("File mode is selected, but uploaded file is not archive.")
+            if basename(normpath(input_file)) in ["meta.json", "key_id_map.json"]:
+                input_dir, input_file = dirname(input_file), None
+            if input_file and (sly.image.has_valid_ext(input_file) or file_ext == ".json"):
+                if dirname(dirname(normpath(input_file))) == "related_images":
+                    possible_dataset_dir = dirname(dirname(dirname(normpath(input_file))))
+                    dataset_listdir = api.file.listdir(g.TEAM_ID, possible_dataset_dir)
+                    if all(
+                        basename(normpath(x)) in ["pointcloud", "ann", "related_images"]
+                        for x in dataset_listdir
+                        if api.file.dir_exists(g.TEAM_ID, x)
+                    ):
+                        possible_project_dir = dirname(normpath(possible_dataset_dir))
+                        project_listdir = api.file.listdir(g.TEAM_ID, possible_project_dir)
+                        if any(basename(normpath(x)) == "meta.json" for x in project_listdir):
+                            sly.logger.info(f"Found meta.json in {possible_project_dir}.")
+                            input_dir, input_file = possible_project_dir, None
+            if input_file and (sly.pointcloud.has_valid_ext(input_file) or file_ext == ".json"):
+                possible_dataset_dir = dirname(dirname(normpath(input_file)))
+                dataset_listdir = api.file.listdir(g.TEAM_ID, possible_dataset_dir)
+                if all(
+                    basename(normpath(x)) in ["pointcloud", "ann", "related_images"]
+                    for x in dataset_listdir
+                    if api.file.dir_exists(g.TEAM_ID, x)
+                ):
+                    possible_project_dir = dirname(normpath(possible_dataset_dir))
+                    project_listdir = api.file.listdir(g.TEAM_ID, possible_project_dir)
+                    if any(basename(normpath(x)) == "meta.json" for x in project_listdir):
+                        sly.logger.info(f"Found meta.json in {possible_project_dir}.")
+                        input_dir, input_file = possible_project_dir, None
+            if input_file and sly.pointcloud.has_valid_ext(input_file):
+                input_dir, input_file = dirname(normpath(input_file)), None
+
+    return input_dir, input_file
 
 
 def download_input_files(api, task_id, input_dir, input_file):
@@ -23,9 +120,10 @@ def download_input_files(api, task_id, input_dir, input_file):
 
         sizeb = api.file.get_directory_size(g.TEAM_ID, input_dir)
         extract_dir = os.path.join(g.storage_dir, cur_files_path.strip("/"))
-        project_name = Path(cur_files_path).name
-        progress_cb = download_progress.get_progress_cb(api, task_id, f"Downloading {g.INPUT_DIR.strip('/')}", sizeb,
-                                                        is_size=True)
+        # project_name = Path(cur_files_path).name
+        progress_cb = download_progress.get_progress_cb(
+            api, task_id, f"Downloading {g.INPUT_DIR.strip('/')}", sizeb, is_size=True
+        )
         api.file.download_directory(g.TEAM_ID, input_dir, extract_dir, progress_cb)
     else:
         if g.IS_ON_AGENT:
@@ -36,19 +134,78 @@ def download_input_files(api, task_id, input_dir, input_file):
         sizeb = api.file.get_info_by_path(g.TEAM_ID, input_file).sizeb
         archive_path = os.path.join(g.storage_dir, sly.fs.get_file_name_with_ext(cur_files_path))
         extract_dir = os.path.join(g.storage_dir, sly.fs.get_file_name(cur_files_path))
-        project_name = sly.fs.get_file_name(input_file)
-        progress_cb = download_progress.get_progress_cb(api, task_id, f"Downloading {g.INPUT_FILE.lstrip('/')}",
-                                                        sizeb,
-                                                        is_size=True)
+        # project_name = sly.fs.get_file_name(input_file)
+        progress_cb = download_progress.get_progress_cb(
+            api, task_id, f"Downloading {g.INPUT_FILE.lstrip('/')}", sizeb, is_size=True
+        )
         api.file.download(g.TEAM_ID, input_file, archive_path, None, progress_cb)
 
-        if tarfile.is_tarfile(archive_path):
-            with tarfile.open(archive_path) as archive:
-                archive.extractall(extract_dir)
-        elif zipfile.is_zipfile(archive_path):
-            z_file = zipfile.ZipFile(archive_path)
-            z_file.extractall(extract_dir)
+        if is_archive_path(archive_path):
+            if tarfile.is_tarfile(archive_path):
+                with tarfile.open(archive_path) as archive:
+                    archive.extractall(extract_dir)
+            elif zipfile.is_zipfile(archive_path):
+                z_file = zipfile.ZipFile(archive_path)
+                z_file.extractall(extract_dir)
+            else:
+                raise Exception(f"Failed to extract archive '{archive_path}'.")
         else:
-            raise Exception(f"Incorrect archive format '{archive_path}'. Please use .tar or .zip files.")
+            raise Exception(
+                f"Incorrect file format '{archive_path}'. Please use .tar or .zip files."
+            )
         sly.fs.silent_remove(archive_path)
-    return extract_dir, project_name
+    return extract_dir
+
+
+def get_project_dirs(input_dir):
+    project_dirs = [project_dir for project_dir in sly.fs.dirs_filter(input_dir, search_projects)]
+
+    only_pcd_dirs = []
+    if len(project_dirs) == 0:
+        only_pcd_dirs = [pcd_dir for pcd_dir in sly.fs.dirs_filter(input_dir, search_pcd_dir)]
+    return project_dirs, only_pcd_dirs
+
+
+def upload_only_pcds(api: sly.Api, task_id, project_name, only_pcd_dirs):
+    project = None
+    pcd_cnt = 0
+    for pcd_dir in only_pcd_dirs:
+        if not sly.fs.dir_exists(pcd_dir):
+            continue
+        pcd_paths = sly.fs.list_files(
+            pcd_dir,
+            valid_extensions=sly.pointcloud.ALLOWED_POINTCLOUD_EXTENSIONS,
+            ignore_valid_extensions_case=True,
+        )
+        if len(pcd_paths) == 0:
+            continue
+        if project is None:
+            project = api.project.create(
+                g.WORKSPACE_ID,
+                project_name,
+                type=sly.ProjectType.POINT_CLOUDS,
+                change_name_if_conflict=True,
+            )
+        total_size = sum([sly.fs.get_file_size(path) for path in pcd_paths])
+        dataset = api.dataset.create(project.id, "ds0", change_name_if_conflict=True)
+        progress_project_cb = get_progress_cb(
+            api,
+            task_id,
+            f"Uploading pointclouds from directory '{pcd_dir}' to dataset '{dataset.name}'",
+            total_size,
+        )
+        pcd_names = [
+            os.path.basename(path) for path in pcd_paths if sly.pointcloud.has_valid_ext(path)
+        ]
+        pointclouds = api.pointcloud.upload_paths(
+            dataset.id, pcd_names, pcd_paths, progress_project_cb
+        )
+        pcd_cnt += len(pointclouds)
+    if pcd_cnt > 1:
+        sly.logger.info(f"{pcd_cnt} pointclouds were uploaded to project '{project.name}'.")
+    elif pcd_cnt == 1:
+        sly.logger.info(f"{pcd_cnt} pointcloud was uploaded to project '{project.name}'.")
+
+    if project is None:
+        return pcd_cnt, None
+    return pcd_cnt, project.id
